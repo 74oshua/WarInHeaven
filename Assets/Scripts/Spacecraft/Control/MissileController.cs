1fa01ed464124d9b0200a2c23cf4a709065cf773
74oshua <joshua.lowry02@gmail.com>
2025-03-30 00:14:51 -1000

    removed dependency on HandleUtilility

--- Assets/Scripts/Spacecraft/Control/MissileController.cs
+++ Assets/Scripts/Spacecraft/Control/MissileController.cs
@@ -1,8 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.Timeline;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(Spacecraft))]
 [RequireComponent(typeof(Scanner))]
